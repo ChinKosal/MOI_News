ca5031573eaa0668b8053bbda15e35f8c6fe031f
Chin Kosal <nangapple82@gmail.com>
2023-11-22 09:25:37 +0700

    added

--- js/index.js
+++ js/index.js
@@ -41,7 +41,7 @@ function setActiveButton(btnNumber) {
 }
 
 // popup
-let headerhide = document.getElementById("");
+// let headerhide = document.getElementById("");
 function openPopup() {
   console.log("success");
   document.getElementById("popup").style.transition = "0.2s all ease-in";
@@ -104,39 +104,6 @@ function toggleDialog() {
       ? "block"
       : "none";
 }
-
-// slider js
-// Get the button element
-// Wait for the DOM to be ready
-document.addEventListener("DOMContentLoaded", function () {
-  // Initialize Swiper
-  var swiper = new Swiper(".swiper-container", {
-    direction: "vertical",
-    slidesPerView: "auto",
-    spaceBetween: 0,
-    loop: true,
-    autoplay: {
-      delay: 2000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    effect: "slide", // Use the slide effect
-    speed: 1000, // Set the transition speed in milliseconds
-  });
-
-  // document.getElementById("nextButton").addEventListener("click", function () {
-  //   swiper.slideNext();
-  // });
-
-  document
-    .getElementById("scrollToTopButton1")
-    .addEventListener("click", function () {
-      swiper.slidePrev();
-    });
-});
 
 // JavaScript function to toggle the visibility of the modal and set the video source
 function toggleModal(cardId) {
